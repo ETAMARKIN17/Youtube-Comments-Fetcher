357f03146bc83925c147ed5c3e8ff8785a31e217
Elliott Tamarkin <tamarkinellie@gmail.com>
2024-06-20 20:42:47 +0000

    Added a keyword search feature and created functions for the search conditions

--- SEOyoutubeAPI.py
+++ SEOyoutubeAPI.py
@@ -6,9 +6,38 @@ import pandas as pd
 API_KEY = "AIzaSyD1AsmB66YJl8RN-YDHbWHBclTBDsjFkDY"
 default_id = "X8VuIq4j1bo"
 
+#function to get the key word to search
+def get_key_word():
+    while True: #loop until a valid response is given
+        search_on_word = input("Would you like to filter based on any key word(s)? type 'y' for yes or 'n' for no: ")
+        #if the user wants to search by a key word get the word
+        if (search_on_word == 'y'): 
+            key_word = input("Enter the desired key word to search for: ")
+            return key_word
+        #if the user does not want to search by a key word return none
+        elif search_on_word == 'n':
+            return None
+        #else keep running the loop until a valid response is given
+        else:
+            print("Invalid response. Enter either 'y' or 'n'.")
+
+#function to get the sorting method
+def get_sorting_method():
+    while True: #loop until a valid response is given
+        sorted_order = input("Enter 't' to filter by the most recent comments or 'r' to filter by relevance: ")
+        #if sorted input is t sort by time
+        if sorted_order == 't': 
+            return "time"
+        #if sorted input is r sort by relevance
+        elif sorted_order == "r":
+            return "relevance"
+        #else keep running the loop until a valid response is given
+        else:
+            print("Invalid sorting method. Enter 't' or 'r'.")
+
 #get a video URL from the user to extract the id
 video_id = ""
-video_url = input("Enter a video URL, type 'd' to use the default video: ")
+video_url = input("Enter a Youtube video URL, type 'd' to use the default video: ")
 
 #if the user inputs 'd', use the default video
 if video_url == 'd':
@@ -16,24 +45,20 @@ if video_url == 'd':
 else:  #else extract the id from the user's URL
     video_id = video_url[32:]
 
-#Set valid varilabe to keep loop until a valid response is given
-valid = False
-filter = "" #initilaize filter variable
+#prompt the user for a key word
+key_word = get_key_word()
+
+#get sorting method
+filter = get_sorting_method()
 
 
-while valid == False:
-    sorted_order = input("Enter a way to sort the comments, use 't' to filter by the most recent comments or 'r' to filter by relevance: ")
-    if (sorted_order == 't'): #if sorted input is t sort by time
-        filter = "time"
-        valid = True
-    elif (sorted_order == "r"): #if sorted input is r sort by relevance
-        filter = "relevance"
-        valid = True
-    else: #else keep running the loop until a valid response is given
-        print("Invalid sorting method please enter either 't' or 'r'")
+#construct the API request URL based on user inputs
+if key_word:
+    url = f"https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId={video_id}&key={API_KEY}&maxResults=20&order={filter}&searchTerms={key_word}"
+else:
+    url = f"https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId={video_id}&key={API_KEY}&maxResults=20&order={filter}"
 
-#use the id and API key to set a GET request for the comments
-url = f"https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId={video_id}&key={API_KEY}&maxResults=20&order={filter}"
+#send GET request to API
 response = requests.get(url)
 
 #print the status of the request
@@ -63,7 +88,6 @@ if response.status_code == 200:
         published.append(published_date)
         likes.append(like_count)
         replies.append(reply_count)
-
 
     #create a new dictionary with the extracted data
     extracted_data = {

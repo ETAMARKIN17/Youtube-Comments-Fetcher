5b6a61538b9c28cf46b3ae84d73a6d94b1543eea
Elliott Tamarkin <tamarkinellie@gmail.com>
2024-06-21 23:10:19 +0000

    added yaml automatic test case file to run on every push

---
+++ ytCommentTester.py
@@ -0,0 +1,25 @@
+'''
+Add to here later for now my functions cannot be 
+tested with this method as they do not take
+any parameters but I am setting it up now for 
+easy future access
+
+THE CODE IS BELOW:
+'''
+
+'''
+import unittest
+from yourCodeFileName import function1, function2
+
+
+class TestFileName(unittest.TestCase):
+    def test_function1(self):
+        self.assertEqual(function1(1), 0)
+
+    def test_function2(self):
+        self.assertEqual(function2(2,1), 3)
+        self.assertEqual(function2(2.1, 1.2), 3.3)
+
+if __name__ == '__main__':
+    unittest.main()
+'''
